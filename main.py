859c7eb04776ca6679ed9304609cf45c178c4ef8
bobyasasas <bobyasasas@outlook.com>
2024-05-16 17:22:15 +0800

    add login and signup function

---
+++ main.py
@@ -0,0 +1,59 @@
+import json
+import os
+from datetime import timedelta
+
+from flask import Flask, request, session
+
+import myutil.mysql
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = "wk6666"
+app.config['PERMANENT_SESSION_LIFETIME'] = timedelta(days=7)
+
+BASE_URL = '/chat/'
+
+
+@app.route(BASE_URL + 'post/signup', methods=['POST'])
+def signup():
+    data = request.get_data()
+    json_data = json.loads(data.decode("utf-8"))
+    username = json_data["username"]
+    passwd = json_data["passwd"]
+    mysql = myutil.mysql.Mydb()
+    sign_result = mysql.add_user(username, passwd)
+    if sign_result == 0:
+        result = {
+            'msg': "false"
+        }
+    else:
+        result = {
+            'msg': "true"
+        }
+    session['username'] = username
+    session.permanent = True
+    result_json = json.dumps(result)
+    return result_json
+
+
+@app.route(BASE_URL + 'post/login', methods=['POST'])
+def login():
+    data = request.get_data()
+    json_data = json.loads(data.decode("utf-8"))
+    username = json_data["username"]
+    passwd = json_data["passwd"]
+    mysql = myutil.mysql.Mydb()
+    if mysql.check_passwd(username, passwd):
+        result = {
+            'msg': "true"
+        }
+        session['username'] = username
+        session.permanent = True
+    else:
+        result = {
+            'msg': "false"
+        }
+    return json.dumps(result)
+
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=8080)
---
+++ myutil/mysql.py
@@ -0,0 +1,46 @@
+import hashlib
+import pymysql.cursors
+from datetime import datetime
+
+
+class Mydb:
+
+    def __init__(self):
+        self.db = pymysql.connect(host='154.204.60.18',
+                                  user='chat_user',
+                                  password='6JE4Rs6GXwk57GGa',
+                                  database='chat_user',
+                                  port=3307,
+                                  charset='utf8')
+
+    def add_user(self, username, passwd):
+        with self.db.cursor() as cursor:
+            check_sql = "SELECT * FROM users WHERE username = %s"
+            cursor.execute(check_sql, (username,))
+            check_result = cursor.fetchall()
+            if len(check_result) == 0:
+                passwd_sha = hashlib.sha256(passwd.encode('utf-8')).hexdigest()
+                time = datetime.now().timestamp()
+                sql = "INSERT INTO users(username, passwd, last_login) VALUES (%s, %s, %s)"
+                cursor.execute(sql, (username, passwd_sha, time))
+                self.db.commit()
+                print("用户", username, "注册成功!")
+                return 1
+            else:
+                print("该账号已经注册过了!")
+                return 0
+
+    def check_passwd(self, username, passwd):
+        with self.db.cursor() as cursor:
+            check_sql = "SELECT * FROM users WHERE username = %s"
+            cursor.execute(check_sql, (username,))
+            check_result = cursor.fetchone()  # 使用 fetchone 而不是 fetchall，因为我们只需要第一个结果
+            if check_result:  # 如果找到了匹配的用户
+                stored_passwd_hash = check_result[2]  # 假设密码哈希在查询结果的某个列中，你需要用正确的列索引替换它
+                given_passwd_hash = hashlib.sha256(passwd.encode('utf-8')).hexdigest()
+                if stored_passwd_hash == given_passwd_hash:
+                    return True
+            return False
+
+    def close(self):
+        self.db.close()
